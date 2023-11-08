f76798b9fd9cd9da1a65b5f0fe4c61432d6847c8
Juliano Sathler <jsathler@live.com>
2023-11-08 11:30:20 -0300

    Added outputs

---
+++ outputs.tf
@@ -0,0 +1,31 @@
+output "service_plan_id" {
+  value = azurerm_service_plan.default.id
+}
+
+output "service_plan_kind" {
+  value = azurerm_service_plan.default.kind
+}
+
+output "std_logic_app_ids" {
+  value = try({ for key, value in azurermazurerm_logic_app_standard.default : value.name => value.id }, null)
+}
+
+output "std_logic_app_identities" {
+  value = try({ for key, value in azurermazurerm_logic_app_standard.default : value.name => value.identity }, null)
+}
+
+output "std_logic_app_default_host_names" {
+  value = try({ for key, value in azurermazurerm_logic_app_standard.default : value.name => value.default_hostname }, null)
+}
+
+output "win_function_ids" {
+  value = try({ for key, value in azurerm_windows_function_app.default : value.name => value.id }, null)
+}
+
+output "win_function_identities" {
+  value = try({ for key, value in azurerm_windows_function_app.default : value.name => value.identity }, null)
+}
+
+output "win_function_default_host_names" {
+  value = try({ for key, value in azurerm_windows_function_app.default : value.name => value.default_hostname }, null)
+}
